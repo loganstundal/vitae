510a2c25c464a2b60098d640d10078108c37a117
Logan Stundal <logan.stundal@gmail.com>
2021-09-08 13:01:43 -0500

    Edits per Nisha job market

---
+++ stundal-cv_knit.R
@@ -0,0 +1,40 @@
+#-----------------------------------------------------------------------------#
+#
+# Author:        Logan Stundal
+# Date:          January 01, 2020
+# Purpose:       Note
+#
+#
+# Copyright (c): Logan Stundal, 2020
+# Email:         stund005@umn.edu
+#
+#-----------------------------------------------------------------------------#
+#
+# Notes:
+#
+#
+#-----------------------------------------------------------------------------#
+
+
+#---------------------------#
+# Set working directory
+#---------------------------#
+setwd('c:/users/logan/googledrive/umn/research/vitae')
+
+
+# SAVE ------------------------------------------------------------------------
+
+# Knit the pdf
+rmarkdown::render(input       = 'stundal-cv.Rmd',
+                  output_file = 'stundal-cv.pdf')
+
+# Save a copy with today's date to the 'Old-Versions' folder
+file.copy(from = "stundal-cv.pdf",
+          to   = paste0("Old-Versions/CV_",format(lubridate::today(), "%Y%m%d"),".pdf"),
+          overwrite = TRUE)
+
+
+# Save a copy to website directory folder for immediate upload
+file.copy(from = "stundal-cv.pdf",
+          to   = "C:/Users/logan/GoogleDrive/UMN/RESEARCH/website/static/media/stundal-cv.pdf",
+          overwrite = TRUE)
